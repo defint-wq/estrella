0d05d35f80494cdd9d9eed595e5b98a6576f07dd
defint-wq <defint.exe@gmail.com>
2026-08-30 00:58:45 +0800

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:3000/graphql" }),
+  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
   cache: new InMemoryCache(),
 });
 
