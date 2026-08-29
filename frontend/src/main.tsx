1d65506e7a0d1d9fc570e9a4995894ea8732622a
defint-wq <defint.exe@gmail.com>
2026-08-30 01:48:24 +0800

    fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+  link: new HttpLink({ 
+    uri: "https://estrella-9q81.onrender.com/graphql" 
+  }),
   cache: new InMemoryCache(),
 });
 
